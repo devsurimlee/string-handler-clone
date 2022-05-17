9544ec39c74f49cde46643fc80fa28162fd51fd3
Surim Lee <devsurimlee@gmail.com>
2022-05-17 15:49:17 +0900

    project init

---
+++ src/main/java/com/handler/stringhandlerclone/StringHandlerCloneApplication.java
@@ -0,0 +1,13 @@
+package com.handler.stringhandlerclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringHandlerCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringHandlerCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handler/stringhandlerclone/StringHandlerCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handler.stringhandlerclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringHandlerCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
