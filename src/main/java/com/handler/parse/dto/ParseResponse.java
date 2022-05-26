284a6215226522f37280bc0396bcc7593c14983d
Surim Lee <devsurimlee@gmail.com>
2022-05-26 16:03:57 +0900

    feat: parseService 완성

--- src/main/java/com/handler/parse/dto/ParseResponse.java
+++ src/main/java/com/handler/parse/dto/ParseResponse.java
@@ -8,4 +8,9 @@ public class ParseResponse {
     private final String quotient;      //몫
     private final String remainder;     //나머지
 
+    public ParseResponse(final OutputUnit outputUnit) {
+        this.quotient = outputUnit.getQuotient();
+        this.remainder = outputUnit.getRemainder();
+    }
+
 }
