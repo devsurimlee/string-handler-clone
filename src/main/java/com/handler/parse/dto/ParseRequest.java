f12ef63b01fb87953251bfa560acd962d0e6b9d8
Surim Lee <devsurimlee@gmail.com>
2022-05-27 14:26:46 +0900

    feat: GlobalExceptionHandler 추가

--- src/main/java/com/handler/parse/dto/ParseRequest.java
+++ src/main/java/com/handler/parse/dto/ParseRequest.java
@@ -4,17 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @RequiredArgsConstructor
 public class ParseRequest {
 
     @Schema(description = "URL", defaultValue = "https://www.naver.com")
+    @Pattern(regexp = "(http)s?:\\/\\/(www\\.)?[a-zA-Z0-9@:%._\\+~#=]+\\.[a-zA-Z0-9@:%._\\/+-~#=?]+", message = "URL 형식이 아닙니다.")
     private final String url;
 
     @Schema(description = "노출유형", defaultValue = "REMOVE_HTML")
     private final ExposureType exposureType;
 
     @Schema(description = "출력묶음단위", defaultValue = "10")
+    @Min(value = 1, message = "1이상의 수를 입력하세요.")
     private final Integer unitCount;
 
 }
