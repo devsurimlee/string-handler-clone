5ebc4a21cf818a662d13ded0461588e3558a703f
Surim Lee <devsurimlee@gmail.com>
2022-05-27 09:42:02 +0900

    setting: Swagger 설정

--- src/main/java/com/handler/parse/controllor/ParseController.java
+++ src/main/java/com/handler/parse/controllor/ParseController.java
@@ -3,6 +3,7 @@ package com.handler.parse.controllor;
 import com.handler.parse.dto.ParseRequest;
 import com.handler.parse.dto.ParseResponse;
 import com.handler.parse.service.ParseService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class ParseController {
 
     private final ParseService parseService;
 
+    @Operation(summary = "URL data process", description = "URL 파싱후 HTML 데이터 가공")
     @PostMapping("api/parse")
     public ResponseEntity<ParseResponse> parse(@RequestBody @Valid ParseRequest request) {
             final ParseResponse response = parseService.parse(request);
